a1635bb626f5e27aeb0913d55539d6abcd906248
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-12-15 00:29:40 +0100

    code: user routes added

--- src/server.ts
+++ src/server.ts
@@ -2,29 +2,25 @@ import express from "express";
 import router from "./router";
 import morgan from "morgan";
 import cors from "cors";
+import { protect } from "./modules/auth";
+
+import { createNewUser, signIn } from "./handlers/user";
 
 const app = express();
-
-function logger(msg) {
-    return (req, res, next) => {
-        console.log(`message: ${msg}`);
-        next();
-    }
-}
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
-app.use(logger("fuck qshd"));
 
 
 app.get("/",(req, res) => {
-    console.log("Hello World");
     res.json({message: "hello world"});
     res.sendStatus(200);
 })
 
-app.use('/api', router);
+app.use('/api', protect, router);
+app.use('/signup', createNewUser);
+app.use('/signin', signIn);
 
 export default app;
