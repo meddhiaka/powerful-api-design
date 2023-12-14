a8c54e7aa104d9317a202b8e0fda7755e0a111eb
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-12-15 00:29:17 +0100

    init: env package init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-import app from './server'
+import app from './server';
+import dotenv from "dotenv";
+dotenv.config();
 
 app.listen(1337, () => {
     console.log("Server running at http://localhost:1337/");
