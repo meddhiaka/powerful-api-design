1b0eeb1f352bca63f26c3cff56468f6c6d276995
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-12-16 00:24:58 +0100

    fix: NODE_ENV feature cancelled

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import app from './server';
 import dotenv from "dotenv";
-import config from "./config"
 dotenv.config();
 
-app.listen(config.port, () => {
-    console.log(`Server running at http://localhost:${config.port}/`);
+app.listen(process.env.port, () => {
+    console.log(`Server running at http://localhost:${process.env.port}/`);
 });
