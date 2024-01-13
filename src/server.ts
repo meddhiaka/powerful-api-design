963291c25360912f4e2fd31c645864aa7ea47d04
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-01-14 00:22:36 +0100

    code: missing semicolons added

--- src/server.ts
+++ src/server.ts
@@ -10,8 +10,8 @@ const app = express();
 
 app.use(cors());
 app.use(morgan("dev"));
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 
 app.get("/",(req, res) => {
