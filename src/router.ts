c5bf511cb86a7ce90a8ea4e70f6d3cc907a1662f
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2024-01-14 12:04:12 +0100

    code: input validation added to update

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
-import {Router} from "express";
+import { Router } from "express";
 import { body, validationResult } from "express-validator";
 import { getProducts, getOneProduct, updateProduct, deleteProduct, createProduct } from "./handlers/product";
+import { validationMiddleware } from "./modules/middleware";
 
 const router = Router();
 
@@ -9,7 +10,7 @@ router.get("/product", getProducts);
 
 router.get("/product/:id", getOneProduct);
 
-router.put("/product/:id", updateProduct);
+router.put("/product/:id", body("name").isString(), validationMiddleware, updateProduct);
 
 router.post("/product", createProduct);
 
@@ -18,34 +19,34 @@ router.delete("/product/:id", deleteProduct);
 
 // update
 router.get("/update", (req, res) => {
-    res.json({message: req.secret_code});
+    res.json({ message: req.secret_code });
 });
 
-router.get("/update/:id", () => {});
+router.get("/update/:id", () => { });
 
-router.put("/update/:id", body("name").isString(), (req, res) => {
-    const errors = validationResult(req);
-    res.status(200).json({errors: errors.array()});
-    if(!errors.isEmpty()) {
-        res.status(400).json({errors: errors.array()});
-    }
-});
+router.put("/update/:id",
+    body("title").optional(),
+    body("body").optional(),
+    body("version").optional(),
+    validationMiddleware,
+    () => { }
+);
 
-router.post("/update", () => {});
+router.post("/update", () => { });
 
-router.delete("/update/:id", () => {});
+router.delete("/update/:id", () => { });
 
 
 // updatepoint
-router.get("/updatepoint", () => {});
+router.get("/updatepoint", () => { });
 
-router.get("/updatepoint/:id", (req, res) => {});
+router.get("/updatepoint/:id", (req, res) => { });
 
-router.put("/updatepoint/:id", () => {});
+router.put("/updatepoint/:id", () => { });
 
 router.post("/updatepoint", () => {});
 
-router.delete("/updatepoint/:id", () => {});
+router.delete("/updatepoint/:id", () => { });
 
 
 export default router;
